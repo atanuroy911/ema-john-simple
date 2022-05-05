f63ed45039fdf3896d8206c9c775d04be917ba69
Atanu Shuvam Roy <atanuroy911@gmail.com>
2022-05-06 01:11:46 +0600

    cart summary fix

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import './Cart.css'
 
-const Cart = ({ cart }) => {
-
+const Cart = ({cart}) => {
     let shipping = 0;
     let total = 0;
+    let quantity = 0;
+    console.log(quantity);
     for(const product of cart){
-        total = total + product.price;
+        console.log(product.quantity);
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
     }
-    const tax = total * 0.1;
-    const grandTotal = total + shipping + parseFloat(tax);
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
         <div className='cart'>
             <div>
                 <h4>Order Summary</h4>
-                <p>Selected Items: {cart.length}</p>
+                <p>Selected Items: {quantity}</p>
             </div>
             <div>
                 <p>Total Price: ${total}</p>
                 <p>Total Shipping: ${shipping}</p>
             </div>
             <div>
-                <p>Tax: ${tax.toFixed(2)}</p>
-                <p><b>Grand Total: ${grandTotal}</b></p>
+                <p>Tax: ${tax}</p>
+                <p><b>Grand Total: ${grandTotal.toFixed(2)}</b></p>
             </div>
         </div>
     );
